d82dd30ec9ce75c08ca5516a875483fd4ee9df84
ib4519894 <ib4519894@lpssonline.com>
2021-08-26 14:55:51 +0000

    Add word_stemming method

--- chat.py
+++ chat.py
@@ -9,28 +9,34 @@ import (
 
 class ChatBot:
     def __init__(self, intents_path):
+        self.stemmer = nltk.stem.lancaster
+        self.words = []
+        self.labels = []
+        self.docs_x = []
+        self.docs_y = []
+
         self.prepare_intents(intents_path)
+        self.word_stemming()
 
     def prepare_intents(self, intents_path):
-
-        stemmer = nltk.stem.lancaster
-
         with open(intents_path, "r") as f:
             data = json.load(f)
         
-        words = []
-        labels = []
-        docs_x = []
-        docs_y = []
-
         for intent in data["intents"]:
             for pattern in intent["patterns"]:
                 wrds = nltk.word_tokenize(pattern)
-                words.extend(wrds)
-                docs_x.append(wrds)
-                docs_y.append(intent["tag"])
+                self.words.extend(wrds)
+                self.docs_x.append(wrds)
+                self.prepare_intentsdocs_y.append(intent["tag"])
             
-            if intent["tag"] not in labels:
-                labels.append(intent["tag"])
+            if intent["tag"] not in self.labels:
+                self.labels.append(intent["tag"])
+    
+    def word_stemming(self):
+        self.words = [self.stemmer.stem(w.lower()) for w in self.words if w != "?"]
+        self.words = sorted(list(set(self.words)))
 
+        self.labels = sorted(self.labels)
+    
+    
 
