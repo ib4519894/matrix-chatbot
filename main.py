e8512bdfa9ccd45d79ecfbe01924d640f2cc9fbb
ib4519894 <ib4519894@lpssonline.com>
2021-08-27 15:02:43 +0000

    Switch tutorial

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import nltk
-nltk.download("punkt")
-nltk.download("wordnet")
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import json
-import pickle
-
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
-import random
+# Based on https://www.upgrad.com/blog/how-to-make-chatbot-in-python/
