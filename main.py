31dff40672ea00b8166ebf8b7c4cdead260a4cd0
ib4519894 <ib4519894@lpssonline.com>
2021-08-31 14:38:30 +0000

    Add corpus training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Based on https://www.upgrad.com/blog/how-to-make-chatbot-in-python/
 import chatterbot
 from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 my_bot = chatterbot.ChatBot(name="placeholder_name", read_only=False, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
 
@@ -19,6 +20,9 @@ responses = (greetings, salutations)
 list_trainer = ListTrainer(my_bot)
 for item in responses:
     list_trainer.train(item)
+
+corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+corpus_trainer.train('chatterbot.corpus.english')
 
 print(my_bot.get_response("bye"))
 while True:
