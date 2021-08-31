4d394cfb6ef2a38046da2a00de10e843787acb04
ib4519894 <ib4519894@lpssonline.com>
2021-08-31 14:35:02 +0000

    Add placeholder interaction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Based on https://www.upgrad.com/blog/how-to-make-chatbot-in-python/
-from chatterbot import Chatbot
+import chatterbot
 from chatterbot.trainers import ListTrainer
 
-my_bot = ChatBot(name="placeholder_name", read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
+my_bot = chatterbot.ChatBot(name="placeholder_name", read_only=False, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
 
 greetings = [
     "hello",
@@ -19,3 +19,7 @@ responses = (greetings, salutations)
 list_trainer = ListTrainer(my_bot)
 for item in responses:
     list_trainer.train(item)
+
+print(my_bot.get_response("bye"))
+while True:
+    print(my_bot.get_response(input("say something > ")))
