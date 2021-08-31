32ea761bcfd0eeac447450cddd5428e2928fb9f6
ib4519894 <ib4519894@lpssonline.com>
2021-08-31 14:12:35 +0000

    Create "my_bot" object

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # Based on https://www.upgrad.com/blog/how-to-make-chatbot-in-python/
+from chatterbot import Chatbot
+from chatterbot.trainers import ListTrainer
+
+my_bot = ChatBot(name="placeholder_name", read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
