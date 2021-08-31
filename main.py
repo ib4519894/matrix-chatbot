70b8663a0f3495180a0c7f3a095bc6de2d5d68ae
ib4519894 <ib4519894@lpssonline.com>
2021-08-31 14:16:22 +0000

    Add training

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ from chatterbot import Chatbot
 from chatterbot.trainers import ListTrainer
 
 my_bot = ChatBot(name="placeholder_name", read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
+
+greetings = [
+    "hello",
+    "hi"
+]
+
+salutations = [
+    "goodbye",
+    "bye"
+]
+
+responses = (greetings, salutations)
+
+list_trainer = ListTrainer(my_bot)
+for item in responses:
+    list_trainer.train(item)
