5c10aa27e4aec8d09643a4a629ab4353d765e1cd
ib4519894 <ib4519894@lpssonline.com>
2021-09-02 14:56:24 +0000

    Change method of bot training

--- bot.py
+++ bot.py
@@ -1,29 +1,23 @@
 import chatterbot
-from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+import os, json
 
 class CustomChatterBot:
     def __init__(self, name="Matrix-Chatbot", learning=True):
         self.chatterbot = chatterbot.ChatBot(name="placeholder_name", read_only=False, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
-        self.response_training_data = {
-            "greetings":["hello", "hi"],
-            "salutations":["bye, goodbye"],
-            "numbers":[str(x) for x in range(0, 10)]
-        }
-        self.corpus_training_data = [
-            "chatterbot.corpus.english"
-        ]
+        self.response_training_data = {}
+        for file in os.listdir("./training_data"):
+            filename = os.fsdecode(file)
+            if filename.endswith(".json"):
+                with open(os.path.join("./training_data", filename)) as f:
+                    new_data = json.loads(f.read())
+                    self.response_training_data = {**self.response_training_data, **new_data}
     
     def train(self):
         self.list_trainer = ListTrainer(self.chatterbot)
         for label, response_list in self.response_training_data.items():
-            print(f"Training {label}")
+            print(f"Training on data: {label}")
             self.list_trainer.train(response_list)
-        
-        corpus_trainer = ChatterBotCorpusTrainer(self.chatterbot)
-        for corpus in self.corpus_training_data:
-            print(f"Training {corpus}")
-            corpus_trainer.train(corpus)
-        
         print("Training Complete")
     
     def respond(self, prompt : str):
