0023c2aac08408957c02b75475021565dcdfd4a6
ib4519894 <ib4519894@lpssonline.com>
2021-09-02 14:57:27 +0000

    Improve status messages

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ prefix = "c!"
 
 @bot.listener.on_message_event
 async def respond_to_chat(room, message):
-    print(f"message {message}")
+    print(f"message - {message}")
     match = botlib.MessageMatch(room, message, bot, prefix)
 
     if match.is_not_from_this_bot() and match.prefix():
-        print("matched")
         response = str(cbot.respond(message.body[2:]))
-        print(f"response - {response}")
         await bot.api.send_text_message(room.room_id, response)
 
 chat_log = []
@@ -30,7 +28,7 @@ async def train_from_chat(room, message):
         data = data[2:]
     chat_log.append(data)
     if len(chat_log) > 15:
-        print(f"training on data {chat_log}")
+        print(f"Training on data: {chat_log}")
         cbot.list_trainer.train(chat_log)
         chat_log = []
 
